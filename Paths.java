7a3498cf17dae66d7de8e71f58680322e58432b8
Ankur Singh <ankurks@thoughtworks.com>
2015-03-09 15:14:50 +0530

    has direct path method created this will take two parmeter first parameter is currentCity and second will be destination city if there is a directpath it will give true else false it is checked for bangalore to singapore

--- Paths.java
+++ Paths.java
@@ -1,23 +1,23 @@
 import java.util.*;
 
 public class Paths{
-	static Map<String,List<String>> routes = new Hashtable<String,List<String>>();
+	static Map<String,List<String>> routes=new Hashtable<String,List<String>>();
 	static{
-		List<String> bangalore = new ArrayList<String>();
+		List<String> bangalore=new ArrayList<String>();
 		bangalore.add("singapore");
-		List<String> singapore = new ArrayList<String>();
+		List<String> singapore=new ArrayList<String>();
 		singapore.add("seoul");
 		singapore.add("bangalore");
 		singapore.add("dubai");
-		List<String> seoul = new ArrayList<String>();
+		List<String> seoul=new ArrayList<String>();
 		seoul.add("beijing");
 		seoul.add("singapore");
-		List<String> beijing = new ArrayList<String>();
+		List<String> beijing=new ArrayList<String>();
 		beijing.add("tokyo");
 		beijing.add("seoul");
-		List<String> tokyo = new ArrayList<String>();
+		List<String> tokyo=new ArrayList<String>();
 		tokyo.add("beijing");
-		List<String> dubai = new ArrayList<String>();
+		List<String> dubai=new ArrayList<String>();
 		tokyo.add("singapore");		
 		routes.put("bangalore",bangalore);
 		routes.put("singapore",singapore);
@@ -28,7 +28,12 @@ public class Paths{
 	}
 
 	public static boolean isCityPresent(String city){
-		Set<String> cities = routes.keySet();
+		Set<String> cities=routes.keySet();
 		return cities.contains(city)?true:false;
+	}
+
+	public static boolean hasDirectPath(String fromCity, String toCity){
+		List<String> startCity=routes.get(fromCity);
+		return (startCity!=null)?startCity.contains(toCity):false;
 	}
 }
--- PathsTest.java
+++ PathsTest.java
@@ -5,55 +5,61 @@ import java.util.*;
 public class PathsTest {
     @Test
     public void test_isCityPresent_gives_true_for_bangalore(){
-        Paths path = new Paths();
+        Paths path=new Paths();
         assertTrue(path.isCityPresent("bangalore"));
     }
 
     @Test
     public void test_isCityPresent_gives_true_for_singapore(){
-        Paths path = new Paths();
+        Paths path=new Paths();
         assertTrue(path.isCityPresent("singapore"));
     }
 
     @Test
     public void test_isCityPresent_gives_true_for_tokyo(){
-        Paths path = new Paths();
+        Paths path=new Paths();
         assertTrue(path.isCityPresent("tokyo"));
     }
 
     @Test
     public void test_isCityPresent_gives_true_for_beijing(){
-        Paths path = new Paths();
+        Paths path=new Paths();
         assertTrue(path.isCityPresent("beijing"));
     }
 
     @Test
     public void test_isCityPresent_gives_true_for_seoul(){
-        Paths path = new Paths();
+        Paths path=new Paths();
         assertTrue(path.isCityPresent("seoul"));
     }
 
     @Test
     public void test_isCityPresent_gives_true_for_dubai(){
-        Paths path = new Paths();
+        Paths path=new Paths();
         assertTrue(path.isCityPresent("dubai"));
     }
 
     @Test
     public void test_isCityPresent_gives_false_for_bakari(){
-        Paths path = new Paths();
+        Paths path=new Paths();
         assertFalse(path.isCityPresent("bakari"));
     }
 
     @Test
     public void test_isCityPresent_gives_false_for_newZeland(){
-        Paths path = new Paths();
+        Paths path=new Paths();
         assertFalse(path.isCityPresent("newZeland"));
     }
 
     @Test
     public void test_isCityPresent_gives_false_for_chennai(){
-        Paths path = new Paths();
+        Paths path=new Paths();
         assertFalse(path.isCityPresent("chennai"));
+    }
+
+    @Test
+    public void test_hasDirectPath_gives_true_for_bangalore_to_singapore(){
+        Paths path=new Paths();
+        assertTrue(path.hasDirectPath("bangalore","singapore"));
     }
 }
